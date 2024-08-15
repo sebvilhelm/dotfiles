b235b8eb3c91203ff6faa2dfccaa53b3b3dfdadf
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2024-08-15 11:15:52 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/llm.lua

--- dot_config/nvim/lua/custom/plugins/llm.lua
+++ dot_config/nvim/lua/custom/plugins/llm.lua
@@ -1,18 +1,56 @@
 return {
+	-- {
+	-- 	"CopilotC-Nvim/CopilotChat.nvim",
+	-- 	branch = "canary",
+	-- 	cmd = {
+	-- 		"CopilotChat",
+	-- 	},
+	-- 	dependencies = {
+	-- 		{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+	-- 		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	-- 	},
+	-- 	opts = {
+	-- 		debug = false, -- Enable debugging
+	-- 		-- See Configuration section for rest
+	-- 	},
+	-- 	-- See Commands section for default commands if you want to lazy load on them
+	-- },
 	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		cmd = {
-			"CopilotChat",
-		},
+		"olimorris/codecompanion.nvim",
 		dependencies = {
-			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
 		},
-		opts = {
-			debug = false, -- Enable debugging
-			-- See Configuration section for rest
-		},
-		-- See Commands section for default commands if you want to lazy load on them
+		config = function()
+			require("codecompanion").setup {
+				adapters = {
+					anthropic = function()
+						return require("codecompanion.adapters").use("anthropic", {
+							env = {
+								api_key = "ANTHROPIC_API_KEY",
+							},
+						})
+					end,
+					openai = function()
+						return require("codecompanion.adapters").use("openai", {
+							env = {
+								api_key = "OPENAI_API_TOKEN",
+							},
+						})
+					end,
+				},
+				strategies = {
+					chat = {
+						adapter = "ollama",
+					},
+					inline = {
+						adapter = "ollama",
+					},
+					agent = {
+						adapter = "anthropic",
+					},
+				},
+			}
+		end,
 	},
 }
