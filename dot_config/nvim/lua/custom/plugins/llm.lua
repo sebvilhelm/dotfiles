286603d8de7c42b3f55c7f721551ff69683bb334
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2024-08-19 15:55:17 +0200

    Update .config/kitty/kitty.conf Update .config/nvim/lazy-lock.json Update .config/nvim/lua/custom/plugins/llm.lua Update .config/nvim/lua/custom/plugins/test.lua Update .config/nvim/plugin/keymaps.lua Update .tmux.conf

--- dot_config/nvim/lua/custom/plugins/llm.lua
+++ dot_config/nvim/lua/custom/plugins/llm.lua
@@ -51,6 +51,26 @@ return {
 					},
 				},
 			}
+
+			-- recommended setup
+			vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap(
+				"n",
+				"<LocalLeader>a",
+				"<cmd>CodeCompanionToggle<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.api.nvim_set_keymap(
+				"v",
+				"<LocalLeader>a",
+				"<cmd>CodeCompanionToggle<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionAdd<cr>", { noremap = true, silent = true })
+
+			-- Expand 'cc' into 'CodeCompanion' in the command line
+			vim.cmd [[cab cc CodeCompanion]]
 		end,
 	},
 }
--- dot_config/nvim/plugin/keymaps.lua
+++ dot_config/nvim/plugin/keymaps.lua
@@ -6,10 +6,11 @@ vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 vim.keymap.set("n", "gL", vim.diagnostic.setloclist)
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- disabled for tmux integration
+-- vim.keymap.set("n", "<C-h>", "<C-w>h")
+-- vim.keymap.set("n", "<C-j>", "<C-w>j")
+-- vim.keymap.set("n", "<C-k>", "<C-w>k")
+-- vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Clear highlights
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")
