a2ab1af4e23a12af0ec268446acaa4e08ef0b163
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2024-08-19 16:01:44 +0200

    Update .config/nvim/lua/custom/plugins/test.lua

--- dot_config/nvim/lua/custom/plugins/test.lua
+++ dot_config/nvim/lua/custom/plugins/test.lua
@@ -6,6 +6,11 @@ return {
 		},
 		config = function()
 			vim.cmd "let test#strategy = 'vimux'"
+
+			vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<CR>")
+			vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>")
+			vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>")
+			vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<CR>")
 		end,
 	},
 }
