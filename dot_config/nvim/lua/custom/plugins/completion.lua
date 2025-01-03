dd72daf150e924508953e84201412ac57cf9a63f
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-01-03 10:51:12 +0100

    Update .config/nvim/lua/custom/plugins/completion.lua

--- dot_config/nvim/lua/custom/plugins/completion.lua
+++ dot_config/nvim/lua/custom/plugins/completion.lua
@@ -35,6 +35,18 @@ return {
 					nerd_font_variant = "mono",
 				},
 
+				completion = {
+					menu = {
+						draw = {
+							columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+						},
+					},
+					-- documentation = {
+					-- 	auto_show = true,
+					-- 	auto_show_delay_ms = 500,
+					-- },
+				},
+
 				signature = { enabled = true },
 
 				snippets = {
