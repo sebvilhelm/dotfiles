8e343dad426ed0f8dfc4da368493d5d9dd1ffeef
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-03-04 09:38:45 +0100

    Update .config/nvim/lua/custom/plugins/completion.lua

--- dot_config/nvim/lua/custom/plugins/completion.lua
+++ dot_config/nvim/lua/custom/plugins/completion.lua
@@ -1,21 +1,4 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		enabled = false,
-		lazy = false,
-		priority = 100,
-		dependencies = {
-			"onsails/lspkind.nvim",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"saadparwaiz1/cmp_luasnip",
-			{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			require "custom.completion"
-		end,
-	},
 
 	{
 		"saghen/blink.cmp",
@@ -71,7 +54,7 @@ return {
 				},
 
 				cmdline = {
-					sources = {},
+					enabled = false,
 				},
 
 				sources = {
