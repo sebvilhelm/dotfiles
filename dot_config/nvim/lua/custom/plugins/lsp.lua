8819c4b2e5d34f3fa00177849d83b16769f89a93
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-09-25 10:20:39 +0200

    Update .config/nvim/lua/custom/lsp.lua Update .config/nvim/lua/custom/plugins/lsp.lua

--- dot_config/nvim/lua/custom/plugins/lsp.lua
+++ dot_config/nvim/lua/custom/plugins/lsp.lua
@@ -1,21 +1,31 @@
 return {
 	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			{ "folke/neodev.nvim", opts = {} },
 			{ "mason-org/mason.nvim", opts = {} },
 			"b0o/SchemaStore.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
 		config = function()
-			require "custom.lsp"
+			require("custom.lsp")
 		end,
 	},
 
 	{
 		"mfussenegger/nvim-lint",
 		config = function(_, opts)
-			local lint = require "lint"
+			local lint = require("lint")
 
 			lint.linters_by_ft = {
 				javascript = { "eslint_d" },
