d7fb85c55b11ab9d47bedbbad627b83ab7c22c4e
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-10-06 09:30:55 +0200

    nvim config and jj config

--- dot_config/nvim/lua/custom/telescope.lua
+++ dot_config/nvim/lua/custom/telescope.lua
@@ -3,35 +3,36 @@ vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc =
 vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Telescope help tags" })
 
 vim.keymap.set("n", "<space>ep", function()
-	require("telescope.builtin").find_files {
+	require("telescope.builtin").find_files({
 		---@diagnostic disable-next-line: param-type-mismatch
-		cwd = vim.fs.joinpath(vim.fn.stdpath "data", "lazy"),
-	}
+		cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+	})
 end, { desc = "Telescope search plugin files" })
 
-require("telescope").setup {
+require("telescope").setup({
 	pickers = {
 		find_files = {
 			theme = "ivy",
+			hidden = true,
 		},
 	},
 	extensions = {
 		fzf = {},
 	},
-}
+})
 
-require("telescope").load_extension "fzf"
+require("telescope").load_extension("fzf")
 
 local live_multigrep = function(opts)
-	local pickers = require "telescope.pickers"
-	local finders = require "telescope.finders"
-	local make_entry = require "telescope.make_entry"
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local make_entry = require("telescope.make_entry")
 	local conf = require("telescope.config").values
 
 	opts = opts or {}
 	opts.cwd = opts.cwd or vim.uv.cwd()
 
-	local finder = finders.new_async_job {
+	local finder = finders.new_async_job({
 		command_generator = function(prompt)
 			if not prompt or prompt == "" then
 				return nil
@@ -50,14 +51,14 @@ local live_multigrep = function(opts)
 			end
 
 			---@diagnostic disable-next-line: deprecated
-			return vim.tbl_flatten {
+			return vim.tbl_flatten({
 				args,
 				{ "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
-			}
+			})
 		end,
 		entry_maker = make_entry.gen_from_vimgrep(opts),
 		cwd = opts.cwd,
-	}
+	})
 
 	pickers
 		.new(opts, {
