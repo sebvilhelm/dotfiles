1804839cfc9ab61c3d47e7c25ac151540fe0fa69
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-10-16 11:01:52 +0200

    Update .config/nvim/lua/custom/dap.lua

--- dot_config/nvim/lua/custom/dap.lua
+++ dot_config/nvim/lua/custom/dap.lua
@@ -1,14 +1,24 @@
-local dap = require "dap"
-local ui = require "dapui"
+local dap = require("dap")
+local ui = require("dapui")
 
 require("dapui").setup()
-require("dap-go").setup {
+require("dap-go").setup({
 	tests = {
 		verbose = true,
 	},
-}
+	-- dap_configurations = {
+	-- 	{
+	-- 		type = "go",
+	-- 		name = "Debug integration test",
+	-- 		request = "launch",
+	-- 		mode = "test",
+	-- 		buildFlags = "-tags=integration",
+	-- 		program = "${fileDirname}",
+	-- 	},
+	-- },
+})
 
-require("nvim-dap-virtual-text").setup {}
+require("nvim-dap-virtual-text").setup({})
 
 -- Handled by nvim-dap-go
 -- dap.adapters.go = {
@@ -22,7 +32,7 @@ require("nvim-dap-virtual-text").setup {}
 
 vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
 vim.keymap.set("n", "<leader>B", function()
-	dap.toggle_breakpoint(vim.fn.input "Breakpoint condition: ")
+	dap.toggle_breakpoint(vim.fn.input("Breakpoint condition: "))
 end)
 
 -- Eval var under cursor
