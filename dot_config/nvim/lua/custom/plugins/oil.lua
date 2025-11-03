30a68738dcbb342f6692795d1092d8235a9830d3
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-11-03 14:41:34 +0100

    Update .config/nvim/lua/custom/plugins/oil.lua

--- dot_config/nvim/lua/custom/plugins/oil.lua
+++ dot_config/nvim/lua/custom/plugins/oil.lua
@@ -3,14 +3,27 @@ return {
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("oil").setup {
+			require("oil").setup({
 				keymaps = {
 					["<C-l>"] = {},
 					["<C-h>"] = {},
 				},
-			}
+				win_options = {
+					signcolumn = "yes:2",
+				},
+			})
 
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 		end,
+	},
+
+	{
+		"refractalize/oil-git-status.nvim",
+
+		dependencies = {
+			"stevearc/oil.nvim",
+		},
+
+		config = true,
 	},
 }
