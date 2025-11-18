940d0e0cea471ece13691ea94f0566d7fbe146d8
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2025-11-18 10:08:19 +0100

    Update .config/nvim/lua/custom/plugins/lsp.lua Update .gitignore_global

--- dot_config/nvim/lua/custom/plugins/lsp.lua
+++ dot_config/nvim/lua/custom/plugins/lsp.lua
@@ -27,12 +27,76 @@ return {
 		config = function(_, opts)
 			local lint = require("lint")
 
-			lint.linters_by_ft = {
-				javascript = { "eslint_d" },
-				javascriptreact = { "eslint_d" },
-				typescript = { "eslint_d" },
-				typescriptreact = { "eslint_d" },
+			lint.linters_by_ft = {}
+
+			local function has_root_config(ctx, names)
+				local search_roots = {
+					ctx.root,
+					ctx.cwd,
+					(ctx.dirname and ctx.dirname ~= "" and ctx.dirname) or vim.fs.dirname(ctx.filename),
+				}
+
+				for _, root in ipairs(search_roots) do
+					if root and root ~= "" then
+						local found = vim.fs.find(names, { path = root, upward = true })
+						if #found > 0 then
+							return true
+						end
+					end
+				end
+
+				return false
+			end
+
+			local js_linters = {}
+
+			local function register_linter(name, config_files)
+				local linter = lint.linters[name]
+				if not linter then
+					return
+				end
+
+				lint.linters[name] = vim.tbl_deep_extend("force", linter, {
+					condition = function(ctx)
+						return has_root_config(ctx, config_files)
+					end,
+				})
+
+				table.insert(js_linters, name)
+			end
+
+			register_linter("eslint_d", {
+				"eslint.config.js",
+				"eslint.config.cjs",
+				"eslint.config.mjs",
+				"eslint.config.ts",
+				".eslintrc",
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.mjs",
+				".eslintrc.ts",
+				".eslintrc.json",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+			})
+
+			-- register_linter("oxlint", {
+			-- 	"oxlint.json",
+			-- 	"oxlint.yaml",
+			-- 	"oxlint.yml",
+			-- 	"oxlint.toml",
+			-- })
+
+			local js_filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
 			}
+
+			for _, ft in ipairs(js_filetypes) do
+				lint.linters_by_ft[ft] = vim.deepcopy(js_linters)
+			end
 
 			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
 				callback = function()
