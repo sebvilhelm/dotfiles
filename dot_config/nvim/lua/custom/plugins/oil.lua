81b4bb5045d2c37bea598938d51a750c82cc0aab
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2026-01-05 10:46:46 +0100

    remove git signs in oil.nvim

--- dot_config/nvim/lua/custom/plugins/oil.lua
+++ dot_config/nvim/lua/custom/plugins/oil.lua
@@ -8,22 +8,9 @@ return {
 					["<C-l>"] = {},
 					["<C-h>"] = {},
 				},
-				win_options = {
-					signcolumn = "yes:2",
-				},
 			})
 
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 		end,
-	},
-
-	{
-		"refractalize/oil-git-status.nvim",
-
-		dependencies = {
-			"stevearc/oil.nvim",
-		},
-
-		config = true,
 	},
 }
