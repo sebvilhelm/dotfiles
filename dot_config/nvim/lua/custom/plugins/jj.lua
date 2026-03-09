2996958c5f24e140b604ce3ee51ef1e816678cea
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2026-03-09 08:44:54 +0100

    remove jj/nvim plugins

--- dot_config/nvim/lua/custom/plugins/jj.lua
+++ dot_config/nvim/lua/custom/plugins/jj.lua
@@ -1,15 +1,5 @@
 return {
 	{
-		"julienvincent/hunk.nvim",
-		dependencies = { "MunifTanjim/nui.nvim" },
-		cmd = { "DiffEditor" },
-		config = function()
-			require("hunk").setup()
-		end,
-	},
-	"rafikdraoui/jj-diffconflicts",
-
-	{
 		"lewis6991/gitsigns.nvim",
 		config = true,
 	},
