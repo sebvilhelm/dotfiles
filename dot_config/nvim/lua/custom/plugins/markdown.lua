730c82f541cb5e2404aadd5fe293c854506c9eb8
Sebastian Vilhelm Juhl <seb.vilhelm@gmail.com>
2026-03-16 13:26:36 +0100

    enable images in nvim

--- dot_config/nvim/lua/custom/plugins/markdown.lua
+++ dot_config/nvim/lua/custom/plugins/markdown.lua
@@ -1,5 +1,16 @@
 return {
 	{
+		"folke/snacks.nvim",
+		lazy = false,
+		priority = 1000,
+		---@type snacks.Config
+		opts = {
+			image = {
+				enabled = true,
+			},
+		},
+	},
+	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" }, -- if you use standalone mini plugins
 		---@module 'render-markdown'
